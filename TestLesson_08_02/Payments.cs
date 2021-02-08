964974fe44f9a4ce383807efe7e7c33fa9fd0030
Kanat Karimov <Karash1985@gmail.com>
2021-02-08 21:03:25 +0600

    Добавьте файлы проекта.

---
+++ TestLesson_08_02/Payments.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestLesson_08_02
+{
+    class Payments
+    {
+    }
+}
---
+++ TestLesson_08_02/Zone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestLesson_08_02
+{
+    public class Zone
+    {
+        public string zona_1; // Зона 1
+        public string zona_2; // Зона 2
+        public string zona_3; // Зона 3
+        public string zona_4; // Зона 4    
+    }
+}
