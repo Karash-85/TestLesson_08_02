42787d14ec18420f5b9160dfe00bb57567e0e051
Kanat Karimov <Karash1985@gmail.com>
2021-02-08 21:52:24 +0600

    Change in ClassPayments

--- TestLesson_08_02/Payments.cs
+++ TestLesson_08_02/Payments.cs
@@ -5,6 +5,26 @@ using System.Text;
 namespace TestLesson_08_02
 {
     class Payments
-    {
+    {   // Наличный расчет ежедневный
+        public enum tariffsCashMethod 
+        {                              // тарифы взрослы, детский, льготный
+            edult = 180,
+            child = 0,
+            preferential = 0,
+        };
+        // Безналичный расчет за месяц
+        public enum tariffsCashlessMethodPerMonth
+        {                              // тарифы взрослы, детский, льготный
+            edult = 7500,
+            child = 0,
+            preferential = 0,
+        };
+        // Безналичный расчет ежедневный
+        public enum tariffsCashlessMethodEveryday
+        {                              // тарифы взрослы, детский, льготный
+            edult = 90,
+            child = 0,
+            preferential = 0,
+        };
     }
 }
